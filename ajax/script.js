bef7c3a0bd925c186684f89e438ba2d5750c83b7
lilijing <lilijing@xiaomi.com>
2015-12-21 14:59:40 +0800

    add ajax

---
+++ ajax/script.js
@@ -0,0 +1,76 @@
+var ajax = {
+	get: function(url, data, success, error) {
+		var xhr = new XMLHttpRequest();
+
+		var str = "";
+		for(key in data) {
+			str +="&" + key + "=" + data[key];
+		}
+		url += "?" + str.substring(1);
+
+		xhr.open('GET', url, true);
+
+		xhr.onreadystatechange = function() {
+			if( xhr.readyState === 4 ) {
+				if( xhr.status === 200 ) {
+					success( JSON.parse(xhr.responseText) );
+				}
+				else {
+					error(xhr.status)
+				}
+			}
+		}
+
+		xhr.send(null)
+	},
+	post: function(url, data, success, error) {
+		var xhr = new XMLHttpRequest();
+
+		xhr.open('POST', url, true);
+
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState === 4) {
+				if( xhr.status === 200 ) {
+					success( JSON.parse(xhr.responseText) );
+				}
+				else {
+					error(xhr.status);
+				}
+			}
+		}
+
+		xhr.setRequestHeader("Content-Type", "application/json");
+
+		xhr.send(data)
+	},
+	file: function(url, data, success, error) {
+		var xhr = new XMLHttpRequest();
+
+		xhr.open('POST', url, true);
+
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState === 4) {
+				if( xhr.status === 200 ) {
+					success( JSON.parse(xhr.responseText) );
+				}
+				else {
+					error(xhr.status);
+				}
+			}
+		}
+
+		var formData = new FormData();
+
+		for(var name in data) {
+
+			if(!data.hasOwnProperty(name)) continue;
+
+			var value = data[name];
+			if(typeof value === 'function') continue;
+
+			formData.append(name, value);
+		}
+
+		xhr.send(formData);
+	}
+}
